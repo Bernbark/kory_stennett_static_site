37719e0e346f471047725b02a7663f68646a58e9
Bernbark <korystennett@yahoo.com>
2022-12-27 10:17:02 -0800

    Added WebGL functionality
    So now I can play my games in the browser

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -3,7 +3,41 @@ import Card from '../components/UI/Card';
 import TwoFrameCard from '../components/UI/TwoFrameCard';
 import LinkSlot from '../components/UI/LinkSlot';
 import Warning from '../components/UI/Warning';
+import React, { useState, useEffect, useCallback } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 function ProjectsPage(){
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "kory_stennett_static_site/UnityBuilds/WebGl.loader.js",
+        dataUrl: "kory_stennett_static_site/UnityBuilds/WebGl.data",
+        frameworkUrl: "kory_stennett_static_site/UnityBuilds/WebGl.framework.js",
+        codeUrl: "kory_stennett_static_site/UnityBuilds/WebGl.wasm",
+      });
+
+      const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
+      const handleChangePixelRatio = useCallback(
+        function () {
+          // A function which will update the device pixel ratio of the Unity
+          // Application to match the device pixel ratio of the browser.
+          const updateDevicePixelRatio = function () {
+            setDevicePixelRatio(window.devicePixelRatio);
+          };
+          // A media matcher which watches for changes in the device pixel ratio.
+          const mediaMatcher = window.matchMedia(
+            `screen and (resolution: ${devicePixelRatio}dppx)`
+          );
+          // Adding an event listener to the media matcher which will update the
+          // device pixel ratio of the Unity Application when the device pixel
+          // ratio changes.
+          mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+          return function () {
+            // Removing the event listener when the component unmounts.
+            mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+          };
+        },
+        [devicePixelRatio]
+      );
     return <div>
         <div>
             <TitleCard>
@@ -11,6 +45,14 @@ function ProjectsPage(){
                     Projects
                 </h1>
             </TitleCard>
+            <TwoFrameCard>
+                <Unity unityProvider={unityProvider} style={{ width: 1200, height: 800 }} devicePixelRatio={devicePixelRatio}/>
+                <Card><h2>Streamer Simulator Idle</h2><br></br><h3>This game definitely has some porting issues, I'm working on it! Hit Tab to open
+                    and close the Chat and Stats windows. It's an idle game where you are a streamer who is
+                    trying to earn enough money and fame to get back to the Intergalactic War with the Stats
+                    and money they earned during their time on Earth.</h3></Card>
+            </TwoFrameCard>
+            
             <section>
             
                     <Card>
